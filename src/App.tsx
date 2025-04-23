9fc6a3a3fd92b88bc3248a77746009dfb6725e7a
BILOSTENKO_D <bilostenkod@gmail.com>
2025-04-23 18:02:27 +0300

    add project, depend., tailwind

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-dark-50 text-accent-red font-mono text-heading-m p-4 text-center font-bold">
+        Tailwind працює як боженька!
+      </div>
+    </>
+  );
+}
+
+export default App;
